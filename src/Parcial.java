1227a832fd09a4bd3f6f284aaa2e2071ac9172cb
Franklin <Franklin@FRANKLIN-99>
2019-03-09 19:32:41 -0500

    Prueba final

--- src/Parcial.java
+++ src/Parcial.java
@@ -1,44 +1,52 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.Random;
+
+import java.io.*;
+
+/**
+ * 
+ * @author Franklin
+ *
+ */
 
 public class Parcial {
 	
 	public static int []Comparacion(int Conduc, int[] Ruta) {
-		
-		int[] Rutas_Asig = new int[Ruta.length];
-		Arrays.sort(Ruta);
-		
+		int[] Rutas_Asig = null;
 		if(Conduc <= Ruta.length) {
+			
+			Rutas_Asig = new int[Ruta.length];
+			Arrays.sort(Ruta);
 			
 			for(int i = 0; i < Ruta.length; i++)
 				Rutas_Asig[i] = Ruta[i];
 		}
 		else {
 			
+			Rutas_Asig = new int[Conduc];
+			Arrays.sort(Ruta);
+			
 			int j = 0;
 			int i = 0;
 			while(i < Conduc) {
 				
-				if(Ruta.length > j) {
+				if(j < Ruta.length) {
 					
 					Rutas_Asig[i] = Ruta[j];
 					i++;
+					j++;
 				}else {
+					
 					j = 0;
 				}
 			}
 			
 		}
 		
-		
 		return Rutas_Asig;
 	}
 	
 	public static void printArray(int[] array, int[] array_1, int Conduc, int Horas) {
-		
 		
 		try
 		{
@@ -48,11 +56,11 @@ public class Parcial {
 				
 				int j = array[i] + array_1[i];
 				if(j > Horas)
-					bw.write("Conductor #" + (i + 1) + "Rutas asignadas: " + array[i] + " , " + array_1[i] + 	
-							 " . Horas Extras: " + (j - Horas));
+					bw.write("** Conductor #" + (i + 1) + ", Rutas asignadas: " + array[i] + " , " + array_1[i] + 	
+							 ", Horas Extras: " + (j - Horas) + ". **");
 				else
-					bw.write("Conductor #" + (i + 1) + "Rutas asignadas: " + array[i] + " , " + array_1[i] + 	
-							 " . Horas Extras: 0");
+					bw.write("** Conductor #" + (i + 1) + ", Rutas asignadas: " + array[i] + " , " + array_1[i] + 	
+							 ", Horas Extras: 0. **");
 				
 				}
 
@@ -63,18 +71,20 @@ public class Parcial {
 		catch(Exception ex) {}		
 	}
 	
-	public static void maind(String args[]) {
+	public static void main(String[] args) {
 		
+		System.gc();
+
 		try
 		{
-			BufferedReader br = new BufferedReader(new InputStreamReader( System.in ));
-			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter( System.out ));
-			int Conductores = Integer.parseInt(br.readLine());
+			BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+			BufferedWriter bw = new BufferedWriter( new OutputStreamWriter( System.out ) );
+			int Conductores = 0;
 			int[] Rutas_M = null;
 			int[] Rutas_T = null;
 			String[] sizes = null;
 			String[] sizes_1 = null;
-			int Horas_Max = Integer.parseInt(br.readLine());
+			int Horas_Max = 0;
 			
 			bw.write("Defina la cantidad de conductores: ");
 			bw.flush();
@@ -92,9 +102,9 @@ public class Parcial {
 			bw.flush();
 			sizes_1 = br.readLine().split(",");
 			
-			Rutas_M = new int[sizes_1.length];
+			Rutas_T = new int[sizes_1.length];
 			for(int i = 0; i < sizes_1.length; i++)
-				Rutas_M[i] = Integer.parseInt( sizes_1[i] );
+				Rutas_T[i] = Integer.parseInt( sizes_1[i] );
 			
 			bw.write("Defina la cantidad de Horas de trabajo ");
 			bw.flush();
@@ -104,6 +114,7 @@ public class Parcial {
 			
 		}
 		catch(Exception ex) {}
+
 	}
 
 }
